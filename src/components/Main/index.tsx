ad66a6bbded903093dd392c44442c5c90b2e7222
Joao Moraes <joao.moraes@gruponzn.com>
2021-04-08 17:29:19 -0300

    Add and config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,12 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Boilerplate Next.Js',
+  description = 'TypeScript, React.Js, Next.Js and Styled Components'
+}) => (
   <S.Wrapper>
-    <S.Title>Boilerplate Next.Js</S.Title>
-    <S.Description>
-      TypeScript, React.Js, Next.Js and Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
   </S.Wrapper>
 )
 
