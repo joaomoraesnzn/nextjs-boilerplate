232315ad21e96b517209e74b27b4c9338d5add64
Joao Moraes <joao.moraes@gruponzn.com>
2021-04-08 15:46:07 -0300

    Update index page and tests

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>Boilerplate Next.Js</h1>
-  </main>
+  <S.Wrapper>
+    <S.Title>Boilerplate Next.Js</S.Title>
+    <S.Description>
+      TypeScript, React.Js, Next.Js and Styled Components
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: 3rem;
+
+  h1 {
+    margin-bottom: 20px;
+  }
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  font-weight: bold;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
